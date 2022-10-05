c952b3cf4cd907e0a7eb0219df35bbf2c4d2c7a0
JOHNNY-22 <johnny.altes1@gmail.com>
2022-10-05 13:06:50 +0200

    Cabecera productos

---
+++ src/components/Client_Header.tsx
@@ -0,0 +1,17 @@
+function Client_Header() {
+  return (
+    <div>
+      <a href="">
+        <img
+          src="img/carrito.png"
+          alt="Carrito-img"
+          className=" h3 w-3rounded-full border border-gray-100 shadow-sm"
+        ></img>
+      </a>
+      <a href="">
+        <img src="img/perfil.png" alt="perfil-img"></img>
+      </a>
+    </div>
+  );
+}
+export default Client_Header;
