5d58cbd6bf983193078e4e6e921f58cd9dabadb4
JOHNNY-22 <johnny.altes1@gmail.com>
2022-10-06 20:31:41 +0200

    displays productos

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,8 +1,26 @@
+import { count } from "console";
 import Image from "next/image";
+import { useState } from "react";
 function Product({ name, imgUrl }: { name: string; imgUrl: string }) {
+  const [weight, setWeight] = useState(100);
+  function incrementClick() {
+    setWeight(weight + 100);
+  }
+  function decrementClick() {
+    setWeight(weight - 100);
+  }
   return (
     <div>
+      <div>
+        <img src={imgUrl} alt="notfound" className="w-20 h-20"></img>
+      </div>
       <h1>{name}</h1>
+      <div>
+        <button onClick={decrementClick}>-</button>
+        <p>{weight} g</p>
+        <button onClick={incrementClick}>+</button>
+        <button>Añadir</button>
+      </div>
     </div>
   );
 }
