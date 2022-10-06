186390a40db41a1bebbb0dbe35efea3bdaad0c94
JOHNNY-22 <johnny.altes1@gmail.com>
2022-10-06 20:34:42 +0200

    Display productos

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,25 +1,27 @@
 import { NextPage } from "next";
 import { trpc } from "../../utils/trpc";
 import Product from "../../components/product/Product";
+import Layout from "../../components/Layout";
 
 const ProductDetails: NextPage = () => {
   const { data } = trpc.useQuery(["product.getAllProducts"]);
   return (
-    <div>
-      {" "}
-      {data ? (
-        data.map((product) => (
-          <Product
-            key={product.id}
-            name={product.name}
-            imgUrl={product.imageURL}
-          ></Product>
-        ))
-      ) : (
-        <p className="text-right">Loading..</p>
-      )}
-      ;
-    </div>
+    <Layout>
+      <div className="grid gap-4 grid-cols-4 grid-rows-3">
+        {data ? (
+          data.map((product) => (
+            <Product
+              key={product.id}
+              name={product.name}
+              imgUrl={product.imageURL}
+            ></Product>
+          ))
+        ) : (
+          <p className="text-right">Loading..</p>
+        )}
+        ;
+      </div>
+    </Layout>
   );
 };
 export default ProductDetails;
