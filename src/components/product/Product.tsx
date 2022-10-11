246eabb63d7e1981bf375fe9e3090dcb1ea50653
aperher <aperher@inf.upv.es>
2022-10-11 09:14:28 +0200

    addproduct to cart mutation

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -23,7 +23,8 @@ function Product({
     }
   }
   function addToCart() {
-    //trpc.useMutation(["cart.addProduct"], {productId: id, amount: weight });
+    const mutation = trpc.useMutation(["cart.addProduct"]);
+    mutation.mutateAsync({ productId: id, amount: weight });
   }
   return (
     <div className=" flex flex-col items-center justify-center py-8 text-center shadow-lg hover:shadow-2xl">
