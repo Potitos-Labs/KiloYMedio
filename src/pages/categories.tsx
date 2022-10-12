47b11c79cf204d59d7af18e14cf924ac919debb4
piliv4 <79758821+piliv4@users.noreply.github.com>
2022-10-12 13:47:40 +0200

    mankeando un pokito :S

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -4,18 +4,37 @@ import Layout from "../components/Layout";
 import Category from "../components/Category";
 
 const ProductDisplay: NextPage = () => {
-  const { data } = trpc.useQuery(["product.getAllEdibleCategories"]);
+  const { data } = trpc.useQuery([
+    "product.getAllEdibleCategories",
+    { getImages: true },
+  ]);
   console.log(data);
   return (
     <Layout>
-      <div className=""> Comestibles </div>
+      <div className="bg-kym3 p-4 font-bold text-white">Comestibles</div>
       <div className="grid grid-cols-2 gap-4 p-12 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
         {data ? (
           data.map((category) => (
             <Category
               key={category.id}
               name={category.categoryInSpanish}
-              imgUrl={"null"}
+              imgUrl="foto"
+              id={category.id}
+            ></Category>
+          ))
+        ) : (
+          <p className="text-right">Cargando...</p>
+        )}
+      </div>
+
+      <div className="bg-kym3 p-4 font-bold text-white">No Comestibles</div>
+      <div className="grid grid-cols-2 gap-4 p-12 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {data ? (
+          data.map((category) => (
+            <Category
+              key={category.id}
+              name={category.categoryInSpanish}
+              imgUrl="foto"
               id={category.id}
             ></Category>
           ))
