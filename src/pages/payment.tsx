6ada54a85cc3db734e96add020e4912e8a487089
piliv4 <79758821+piliv4@users.noreply.github.com>
2022-10-19 16:42:49 +0200

    Cambios en la movida del paymentGateway (DISSABLED LAS KEYS)

--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -1,13 +1,52 @@
 import { NextPage } from "next";
 import Layout from "../components/Layout";
 import { trpc } from "../utils/trpc";
-import PaymentGateway from "../components/PaymentGateway";
+import PaymentGateway from "../components/payment/PaymentGateway";
 import Image from "next/image";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { IoTrashOutline } from "react-icons/io5";
+import { useMultistepFrom } from "../components/payment/useMultistepForm";
+import { randomInt } from "crypto";
+
+type FormData = {
+  firstName: string;
+  surName: string;
+  city: string;
+  creditCardNumber: string;
+  CVV: string;
+  expirationDate: string;
+};
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  surName: "",
+  city: "",
+  creditCardNumber: "",
+  CVV: "",
+  expirationDate: "",
+};
 
 const Payment = () => {
-  const { data } = trpc.useQuery(["cart.getAllCartProduct"]);
+  const [data, setData] = useState(INITIAL_DATA);
+  const { data: cartProduct } = trpc.useQuery(["cart.getAllCartProduct"]);
+
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultistepFrom([
+      // eslint-disable-next-line react/jsx-key
+      <PaymentGateway {...data} updateFields={updateFields} />,
+    ]);
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!isLastStep) return next();
+    alert("Sucessful Account Creation");
+  }
 
   return (
     <Layout>
@@ -18,12 +57,39 @@ const Payment = () => {
             {/*Shopping cart*/}
             <div className="ml-12 h-full border-2 border-solid border-black">
               <div className="bg-gray-400 pb-3">
-                <h1 className="pt-3 pl-3 text-3xl font-bold">
-                  Carrito de compra
-                </h1>
+                <h1 className="pt-3 pl-3 text-3xl font-bold">-MI COMPRA-</h1>
               </div>
               <div className="m-0 grid gap-4 p-4">
-                <PaymentGateway></PaymentGateway>
+                <form onSubmit={onSubmit}>
+                  <div className="font-border flex justify-end">
+                    {currentStepIndex + 1}/ {steps.length}
+                  </div>
+                  {step}
+                  <div
+                    style={{
+                      marginTop: "1rem",
+                      display: "flex",
+                      gap: ".5rem",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    {!isFirstStep && (
+                      <button
+                        type="button"
+                        onClick={back}
+                        className="border-none bg-button px-3 py-1 font-bold text-white hover:bg-button_hover"
+                      >
+                        Back
+                      </button>
+                    )}
+                    <button
+                      type="submit"
+                      className="border-none bg-button px-3 py-1 font-bold text-white hover:bg-button_hover "
+                    >
+                      {!isLastStep ? "Next" : "Finish"}
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </section>
@@ -40,8 +106,8 @@ const Payment = () => {
                   {/* Bill -> Products */}
                   <h2 className="p-3 text-xl font-bold">Productos:</h2>
                   <div className="m-0 grid gap-4 pl-6 pr-3">
-                    {data ? (
-                      data.map((cartProduct) => (
+                    {cartProduct ? (
+                      cartProduct.map((cartProduct) => (
                         <div key={cartProduct.productId}>
                           <div className="grid grid-cols-[50%_30%_20%] items-center">
                             <div className="capitalize">
