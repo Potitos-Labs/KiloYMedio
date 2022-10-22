b3139095f885cd40ec9a87a50b35afa2ccb98c7d
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-10-22 22:05:26 +0200

    validación de registro de usuario

--- src/utils/validations/auth.ts
+++ src/utils/validations/auth.ts
@@ -1,12 +1,32 @@
+import isEmail from "validator/lib/isEmail";
+import isStrongPassword from "validator/lib/isStrongPassword";
 import * as z from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(4).max(25),
+  email: z.string().email({ message: "Email invalido" }),
+  password: z.string().refine(
+    (value) =>
+      isStrongPassword(value, {
+        minLength: 6,
+        minLowercase: 1,
+        minNumbers: 1,
+        minSymbols: 0,
+        minUppercase: 0,
+      }),
+    {
+      message:
+        "La contraseña debe tener como mínimo una la longitud de 6, una minúscula y un número",
+    },
+  ),
 });
 
 export const signUpSchema = loginSchema.extend({
-  username: z.string(),
+  username: z
+    .string()
+    .min(3, { message: "El nombre tiene que tener como mínimo 3 caracteres" })
+    .max(20, {
+      message: "El nombre tiene que tener como máximo 20 caracteres",
+    }),
 });
 
 export const signUpByAdminSchema = signUpSchema.extend({
