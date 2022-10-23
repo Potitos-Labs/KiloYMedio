ec44cc198e790e1b480de3e83bbd07e46cc4b56e
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-10-23 14:11:45 +0200

    trpc middleware

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -33,6 +33,43 @@ const isAuthed = t.middleware(({ ctx, next }) => {
 });
 
 /**
+ * Reusable middleware to ensure
+ * users are Client role
+ */
+const isClient = t.middleware(({ ctx, next }) => {
+  if (!ctx.session || !ctx.session.user || ctx.session.user.role != "client") {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return next({
+    ctx: {
+      ...ctx,
+      // infers that `session` is non-nullable to downstream resolvers
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  });
+});
+
+/**
+ * Reusable middleware to ensure
+ * users are Admin role
+ */
+const isAdmin = t.middleware(({ ctx, next }) => {
+  if (!ctx.session || !ctx.session.user || ctx.session.user.role != "admin") {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return next({
+    ctx: {
+      ...ctx,
+      // infers that `session` is non-nullable to downstream resolvers
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  });
+});
+
+/**
  * Protected procedure
  **/
 export const protectedProcedure = t.procedure.use(isAuthed);
+
+export const clientProcedure = t.procedure.use(isClient);
+export const adminProcedure = t.procedure.use(isAdmin);
