830be3678611164d93d5f41f7a3a2c7c4a7f3a8a
piliv4 <79758821+piliv4@users.noreply.github.com>
2022-10-28 12:39:55 +0200

    recipe card

---
+++ src/components/recipe/RecipeCard.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Stars from "../Stars";
+
+const RecipeCard = ({
+  id,
+  name,
+
+  ratings,
+}: {
+  id: string;
+  name: string;
+
+  ratigs: number;
+}) => {
+  return (
+    <div className="relative flex flex-col items-center justify-center rounded-md py-8 text-center shadow-lg hover:shadow-kym4">
+      <div className="py-3">
+        <Link href={`/product/${id}`}>
+          <a></a>
+        </Link>
+      </div>
+
+      <p className="pb-2 font-semibold text-kym4 first-letter:uppercase">
+        {name}
+      </p>
+      <div>
+        <Stars average={ratings}></Stars>
+      </div>
+    </div>
+  );
+};
+export default RecipeCard;
---
+++ src/pages/recipes/index.tsx
@@ -0,0 +1,5 @@
+const recipes = () => {
+  return <div></div>;
+};
+
+export default recipes;
