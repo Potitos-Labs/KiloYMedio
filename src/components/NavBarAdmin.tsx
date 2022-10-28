bc2eba22fabae79e0f076e90395f722dbcb6a477
Marta López <martalopeez88@gmail.com>
2022-10-28 19:04:57 +0200

    retoques navbar admin

--- src/components/NavBarAdmin.tsx
+++ src/components/NavBarAdmin.tsx
@@ -9,7 +9,7 @@ import { AiOutlineUserAdd } from "react-icons/ai";
 
 function NavBarAdmin() {
   return (
-    <nav className="flex w-full flex-row gap-4 text-lg">
+    <nav className="flex w-full gap-4 text-lg">
       <div className="group relative">
         <Link href={`/product`}>
           <button className="peer flex flex-row items-center py-2 font-semibold text-white hover:text-kym4">
@@ -18,9 +18,9 @@ function NavBarAdmin() {
           </button>
         </Link>
         <div className="absolute z-10 hidden group-hover:block">
-          <div className="flex w-[220px] flex-col rounded-md bg-white py-4 text-kym4 shadow-sm shadow-kym4">
+          <div className="flex w-[220px] flex-col rounded-md bg-white text-kym4 shadow-sm shadow-kym4">
             <Link href={`/product/create`}>
-              <a className="flex flex-row px-5 py-3 hover:bg-background">
+              <a className="flex flex-row px-5 py-3 hover:rounded-md hover:bg-background">
                 <RiMenuAddLine className="mr-1 h-6 w-6 fill-kym2" />
                 Añadir productos
               </a>
@@ -37,33 +37,27 @@ function NavBarAdmin() {
                 Editar categorías
               </a>
             </Link>
-            <Link href={`/admin/register`}>
-              <a className="flex flex-row px-5 py-3 hover:bg-background">
-                <AiOutlineUserAdd className="mr-1 h-6 w-6 fill-kym2" />
-                Crear usuarios
-              </a>
-            </Link>
           </div>
         </div>
       </div>
 
       <div className="group relative">
-        <Link href={`/`}>
-          <button className="peer flex flex-row items-center py-2 font-semibold text-white hover:text-kym4">
+        <Link href={`/recipes`}>
+          <button className="peer ml-4 flex flex-row items-center py-2 font-semibold text-white hover:text-kym4">
             Recetas
             <RiArrowDropDownLine className="h-9 w-9" />
           </button>
         </Link>
         <div className="absolute z-10 hidden group-hover:block">
-          <div className="flex w-[220px] flex-col rounded-md bg-white py-4 text-kym4 shadow-sm shadow-kym4">
-            <Link href={`/`}>
-              <a className="flex flex-row px-5 py-3 hover:bg-background">
+          <div className="flex w-[220px] flex-col rounded-md bg-white text-kym4 shadow-sm shadow-kym4">
+            <Link href={`/recipe/create`}>
+              <a className="flex flex-row px-5 py-3 hover:rounded-md hover:bg-background">
                 <RiMenuAddLine className="mr-1 h-6 w-6 fill-kym2" />
                 Añadir recetas
               </a>
             </Link>
-            <Link href={`/`}>
-              <a className="flex flex-row px-5 py-3 hover:bg-background">
+            <Link href={`/recipes`}>
+              <a className="flex flex-row px-5 py-3 hover:rounded-md hover:bg-background">
                 <RiListUnordered className="mr-1 h-6 w-6 fill-kym2" />
                 Ver recetas
               </a>
@@ -75,6 +69,23 @@ function NavBarAdmin() {
       <button className="px-5 py-2 font-semibold text-white hover:text-kym4">
         Talleres
       </button>
+
+      <div className="group relative">
+        <button className="peer ml-4 flex flex-row items-center py-2 font-semibold text-white hover:text-kym4">
+          Usuarios
+          <RiArrowDropDownLine className="h-9 w-9" />
+        </button>
+        <div className="absolute z-10 hidden group-hover:block">
+          <div className="flex w-[220px] flex-col rounded-md bg-white text-kym4 shadow-sm shadow-kym4">
+            <Link href={`/admin/register`}>
+              <a className="flex flex-row px-5 py-3 hover:rounded-md hover:bg-background">
+                <AiOutlineUserAdd className="mr-1 h-6 w-6 fill-kym2" />
+                Crear usuarios
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
 
       <button className="px-5 py-2 font-semibold text-white hover:text-kym4">
         Soporte
