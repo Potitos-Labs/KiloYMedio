22a6cd0bf739b90ed0831e7bb027bc1e6ae9507a
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-10-30 18:05:18 +0100

    update client method

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -4,12 +4,19 @@ import {
   signUpByAdminSchema,
   signUpSchema,
 } from "../../../utils/validations/auth";
-import { router, publicProcedure, adminProcedure } from "../trpc";
+import {
+  router,
+  publicProcedure,
+  adminProcedure,
+  clientProcedure,
+} from "../trpc";
 import { z } from "zod";
+import { clientSchema } from "../../../utils/validations/client";
 
 export const userRouter = router({
   getClientById: publicProcedure
     .input(z.object({ id: z.string() }))
+    .output(clientSchema)
     .query(async ({ ctx, input: { id } }) => {
       const client = await ctx.prisma.user.findFirst({
         where: { id },
@@ -29,15 +36,36 @@ export const userRouter = router({
         },
       });
 
-      if (client) {
-        const { Client: clientAttr, ...userAttr } = client;
-        return { ...userAttr, ...clientAttr };
+      if (!client || !client.Client) {
+        throw new trpc.TRPCError({ code: "BAD_REQUEST" });
       }
-      return null;
+      const { Client: clientAttr, ...userAttr } = client;
+      return { ...userAttr, ...clientAttr };
     }),
   getAllUsers: publicProcedure.query(async ({ ctx }) => {
     return await ctx.prisma.user.findMany();
   }),
+  updateClient: clientProcedure
+    .input(clientSchema)
+    .mutation(async ({ ctx, input }) => {
+      const { email, name, CP, address, location, image, nif, phoneNumber } =
+        input;
+      await ctx.prisma.user.update({
+        where: { id: ctx.session.user.id },
+        data: {
+          name,
+          email,
+          image,
+          nif,
+          Client: { update: { CP, address, location, phoneNumber } },
+        },
+      });
+
+      return {
+        status: 201,
+        message: "Account updated successfully",
+      };
+    }),
   createNewClient: publicProcedure
     .input(signUpSchema)
     .mutation(async ({ input, ctx }) => {
