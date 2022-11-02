7c73f12c468813f64bf347f9db922cf3afd28f52
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-11-02 21:45:12 +0100

    analizador de next

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
@@ -10,53 +12,54 @@ function defineNextConfig(config) {
   return config;
 }
 
-export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-  // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "www.atida.com",
-      "as01.epimg.net",
-      "estaticos.muyinteresante.es",
-      "www.mundorganico.co",
-      "www.adonianatur.com",
-      "http2.mlstatic.com",
-      "cdn.shopify.com",
-      "images.pexels.com",
-      "i.pinimg.com",
-      "cdn2.iconfinder.com",
-      "kilo-y-medio.s3.eu-west-3.amazonaws.com",
-      "www.pngall.com",
-      "assets.stickpng.com",
-      "sgfm.elcorteingles.es",
-      "assets.jumpseller.com",
-      "images.vexels.com",
-      "us.123rf.com",
-      "www.pngall.com",
-      "img.freepik.com",
-      "www.elhombredelsaco.eu",
-      "us.123rf.com",
-      "images-na.ssl-images-amazon.com",
-      "www.vilmupa.com",
-      "i.etsystatic.com",
-      "los40.com",
-      "ecologiautil.com",
-      "www.frutasluisi.eu",
-      "uxwing.com",
-      "pbs.twimg.com",
-      "www.kindpng.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: true,
 });
+
+export default withBundleAnalyzer(
+  defineNextConfig({
+    reactStrictMode: true,
+    swcMinify: true,
+    // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
+    i18n: {
+      locales: ["en"],
+      defaultLocale: "en",
+    },
+    images: {
+      domains: [
+        "lh3.googleusercontent.com",
+        "www.atida.com",
+        "as01.epimg.net",
+        "estaticos.muyinteresante.es",
+        "www.mundorganico.co",
+        "www.adonianatur.com",
+        "http2.mlstatic.com",
+        "cdn.shopify.com",
+        "images.pexels.com",
+        "i.pinimg.com",
+        "cdn2.iconfinder.com",
+        "kilo-y-medio.s3.eu-west-3.amazonaws.com",
+        "www.pngall.com",
+        "assets.stickpng.com",
+        "sgfm.elcorteingles.es",
+        "assets.jumpseller.com",
+        "images.vexels.com",
+        "us.123rf.com",
+        "www.pngall.com",
+        "img.freepik.com",
+        "www.elhombredelsaco.eu",
+        "us.123rf.com",
+        "images-na.ssl-images-amazon.com",
+        "www.vilmupa.com",
+        "i.etsystatic.com",
+        "los40.com",
+        "ecologiautil.com",
+        "www.frutasluisi.eu",
+        "uxwing.com",
+        "pbs.twimg.com",
+        "www.kindpng.com",
+      ],
+    },
+  }),
+);
