1feb11da6fdeb4cbec871c0464e2e3b9b451037e
Alicia Grao <agramel@inf.upv.es>
2022-11-02 08:42:30 +0100

    recetas que fallan pero con linda personalidad

---
+++ src/components/components/IncDecRecipe.tsx
@@ -0,0 +1,73 @@
+import {
+  UseFormGetValues,
+  UseFormRegisterReturn,
+  UseFormSetValue,
+} from "react-hook-form";
+
+import { ICreateRecipe } from "../../utils/validations/recipe";
+
+function IncDecRecipe({
+  maximum,
+  property,
+  register,
+  getValues,
+  setValue,
+}: {
+  maximum: number;
+  property:
+    | "portions"
+    | "timeSpan.hour"
+    | "timeSpan.minute"
+    | "ingredients.amount";
+  register: UseFormRegisterReturn<string>;
+  getValues: UseFormGetValues<ICreateRecipe>;
+  setValue: UseFormSetValue<ICreateRecipe>;
+}) {
+  const getVal = Number(getValues(property));
+  const min = 0;
+
+  //   const maximumValues = {
+  //     portion: 15,
+  //     hour: 23,
+  //     minute: 59,
+  //     ingredient: 999,
+  //   };
+
+  const max = maximum;
+  const length = max.toString.length + 1;
+
+  return (
+    <div className="flex flex-row border-[1px] border-solid border-black">
+      <button
+        disabled={(getVal || 0) == min}
+        className={`border-r-[1px] border-black bg-transparent px-3 font-semibold ${
+          getValues(property) == min ? "cursor-not-allowed opacity-60" : ""
+        }`}
+        onClick={() => {
+          setValue(property, getVal - 1);
+        }}
+      >
+        -
+      </button>
+      <input
+        className="w-16 text-center focus-within:outline-none"
+        type="text"
+        maxLength={length}
+        {...register}
+      />
+      <button
+        disabled={getVal >= max}
+        className={`border-l-[1px] border-black bg-transparent px-3 font-semibold  ${
+          getVal >= max ? "cursor-not-allowed opacity-60" : ""
+        }`}
+        onClick={() => {
+          setValue(property, (getVal || 0) + 1);
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+export default IncDecRecipe;
--- src/pages/recipe/create.tsx
+++ src/pages/recipe/create.tsx
@@ -3,141 +3,136 @@ import { NextPage } from "next";
 import { useForm } from "react-hook-form";
 
 import Layout from "../../components/Layout";
+import IncDecRecipe from "../../components/components/IncDecRecipe";
 import {
   ICreateRecipe,
   createRecipeSchema,
 } from "../../utils/validations/recipe";
 
-// import { RecipeDifficulty } from "@prisma/client";
-// import { trpc } from "../../utils/trpc";
-
 const RecipeForm: NextPage = () => {
   //   const { data: allProducts } = trpc.product.getAllProducts.useQuery();
-  const { register, /*setValue, handleSubmit,*/ watch } =
+  const { register, setValue, getValues, /* handleSubmit,*/ watch } =
     useForm<ICreateRecipe>({
       resolver: zodResolver(createRecipeSchema),
+      defaultValues: { portions: 1 },
     });
 
   console.log(watch());
-
-  //   const eCategory = z.nativeEnum(RecipeDifficulty).parse(difficulty);
-  //   setValue("Edible.category", eCategory);
 
   return (
     <Layout>
       <section>
         <div className="mt-12 pl-10">
-          <form>
-            {/*Details*/}
-            <section className="flex flex-col gap-6">
-              <div className="flex flex-row items-center gap-2">
-                <div className="text-lg">Título </div>
-                <input
-                  autoFocus
-                  className="peer rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
-                  type="text"
-                  placeholder="Título"
-                  {...register("name")}
-                />
-              </div>
-              <div className="flex flex-row items-center gap-2">
-                <div className="text-lg">Dificultad </div>
-                <div className="flex flex-row gap-2">
-                  {/* <select {...register("difficulty", { required: true })}>
+          {/* <form> */}
+          {/*Details*/}
+          <section className="flex flex-col gap-6">
+            <div className="flex flex-row items-center gap-2">
+              <div className="text-lg">Título </div>
+              <input
+                autoFocus
+                className="peer rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
+                type="text"
+                placeholder="Título"
+                {...register("name")}
+              />
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <div className="text-lg">Dificultad </div>
+              <div className="flex flex-row gap-2">
+                {/* <select {...register("difficulty", { required: true })}>
                 <option value="easy">Fácil</option>
                 <option value="moderate">Normal</option>
                 <option value="hard">Difícil</option>
               </select> */}
-                  <input
-                    {...register("difficulty")}
-                    type="radio"
-                    value="easy"
-                  />
-                  <p>Baja</p>
-                  <input
-                    {...register("difficulty")}
-                    type="radio"
-                    value="moderate"
-                  />
-                  <p>Media</p>
-                  <input
-                    {...register("difficulty")}
-                    type="radio"
-                    value="hard"
-                  />
-                  <p>Alta</p>
-                </div>
-              </div>
-              <div className="flex flex-row items-center gap-2">
-                <div className="text-lg">Duración </div>
+                <input {...register("difficulty")} type="radio" value="easy" />
+                <p>Baja</p>
                 <input
-                  className="peer w-[5%] rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
-                  type="number"
-                  {...register("timeSpan.hour", {})}
+                  {...register("difficulty")}
+                  type="radio"
+                  value="moderate"
                 />
-                <p>horas</p>
-                <input
-                  className="peer w-[5%] rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
-                  type="number"
-                  {...register("timeSpan.minute", {})}
-                />
-                <p>minutos</p>
+                <p>Media</p>
+                <input {...register("difficulty")} type="radio" value="hard" />
+                <p>Alta</p>
               </div>
-              <div className="flex flex-row items-center gap-2">
-                <div className="text-lg">Raciones </div>
-                <input
-                  className="peer w-[5%] rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
-                  type="number"
-                  {...register("portions", {})}
-                />
-              </div>
-              <div className="flex flex-row items-center gap-2">
-                <button className="border-2 border-black">Añadir imagen</button>
-                <button className="border-2 border-black">
-                  Cambiar imagen
-                </button>
-                {/* <input type="url" placeholder="img" {...register("img", {})} /> */}
-              </div>
-            </section>
-            {/*End Details*/}
-            {/*Recipe*/}
-            <section>
-              <div>
-                <div className="text-lg">Descripción </div>
-                <textarea
-                  className="peer rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
-                  {...register("description", {})}
-                />
-              </div>
-              <div>
-                <div className="text-lg">Ingredientes </div>
-                <input
-                  type="text"
-                  placeholder="sdfdsfsefae"
-                  {...register("ingredients.name", {})}
-                />
-                <input
-                  className="peer w-[5%] rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
-                  type="number"
-                  {...register("ingredients.amount", {})}
-                />
-                <select {...register("ingredients.unit", { required: true })}>
-                  <option value="Mr">Taza</option>
-                  <option value="Mrs">Gramos</option>
-                  <option value="Miss">Cucharada</option>
-                  <option value="Dr">Etc</option>
-                </select>
-              </div>
-              <div>
-                <div className="text-lg">Pasos </div>
-                <textarea
-                  className="peer rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
-                  {...register("directions", {})}
-                />
-              </div>
-            </section>
-            {/*End Recipe*/}
-          </form>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <div className="text-lg">Duración </div>
+              <IncDecRecipe
+                maximum={23}
+                property={"timeSpan.hour"}
+                register={register("timeSpan.hour")}
+                getValues={getValues}
+                setValue={setValue}
+              ></IncDecRecipe>
+              <p>horas</p>
+              <IncDecRecipe
+                maximum={59}
+                property={"timeSpan.minute"}
+                register={register("timeSpan.minute")}
+                getValues={getValues}
+                setValue={setValue}
+              ></IncDecRecipe>
+              <p>minutos</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <div className="text-lg">Raciones </div>
+              <IncDecRecipe
+                maximum={15}
+                property={"portions"}
+                register={register("portions")}
+                getValues={getValues}
+                setValue={setValue}
+              ></IncDecRecipe>
+            </div>
+
+            <div className="flex flex-row items-center gap-2">
+              <button className="border-2 border-black">Añadir imagen</button>
+              <button className="border-2 border-black">Cambiar imagen</button>
+              {/* <input type="url" placeholder="img" {...register("img", {})} /> */}
+            </div>
+          </section>
+          {/*End Details*/}
+          {/*Recipe*/}
+          <section>
+            <div>
+              <div className="text-lg">Descripción </div>
+              <textarea
+                className="peer rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
+                {...register("description", {})}
+              />
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <div className="text-lg">Ingredientes </div>
+              <input
+                type="text"
+                placeholder="sdfdsfsefae"
+                {...register("ingredients.name", {})}
+              />
+              <IncDecRecipe
+                maximum={999}
+                property={"ingredients.amount"}
+                register={register("ingredients.amount")}
+                getValues={getValues}
+                setValue={setValue}
+              ></IncDecRecipe>
+              <select {...register("ingredients.unit", { required: true })}>
+                <option value="Mr">Taza</option>
+                <option value="Mrs">Gramos</option>
+                <option value="Miss">Cucharada</option>
+                <option value="Dr">Etc</option>
+              </select>
+            </div>
+            <div>
+              <div className="text-lg">Pasos </div>
+              <textarea
+                className="peer rounded-md border-2 border-gray-300 py-2 pl-3 pr-2 placeholder-gray-300"
+                {...register("directions", {})}
+              />
+            </div>
+          </section>
+          {/*End Recipe*/}
+          {/* </form> */}
         </div>
       </section>
     </Layout>
