2f186b71843f073abcf6e0f56545ba69b41de457
piliv4 <79758821+piliv4@users.noreply.github.com>
2022-11-07 22:00:53 +0100

    carrito rediseño

--- src/components/cart/DropDownCart.tsx
+++ src/components/cart/DropDownCart.tsx
@@ -6,28 +6,34 @@ function DropDownCart() {
   const { data: myCart } = trpc.cart.getAllCartProduct.useQuery();
   const numberCartProducts = myCart?.productList.length ?? 0;
   return (
-    <div className=" absolute right-0 z-10 hidden group-hover:block">
-      <div className=" flex h-[200px]  w-[350px] flex-col overflow-y-scroll rounded-md bg-white  text-kym4 shadow-sm shadow-kym4">
-        <div>
-          <p className="text-md mb-5 bg-background pl-3">
-            {numberCartProducts} productos en la cesta
-          </p>
+    <div className=" absolute right-0 z-10 hidden group-hover:block ">
+      <div className="scrollbar-hide flex h-[200px]  w-[350px] flex-col overflow-y-scroll rounded-md bg-white   text-black shadow-sm shadow-kym4">
+        <div className="relative  bg-background pt-3 pl-3">
+          <div className="flex">
+            <p className="text-md mb-5">
+              {numberCartProducts}{" "}
+              {numberCartProducts > 1 ? "productos" : "producto"} en la cesta
+            </p>
+            <p className="absolute right-2 top-2 text-lg font-semibold">
+              {myCart?.totalPrice} €
+            </p>
+          </div>
+          <div className="flex flex-col px-3 pb-3 font-medium ">
+            <Link
+              className="flex flex-col px-3 pb-3 font-medium "
+              href={"/cart"}
+            >
+              <a className="m-2 rounded-md border border-button bg-white text-center text-kym4 hover:bg-button hover:text-white">
+                Ver cesta
+              </a>
+            </Link>
+          </div>
         </div>
-        <div className="flex flex-col px-2 pb-2 font-medium ">
-          <p className="py-2">Total: {myCart?.totalPrice} €</p>
-          <Link href={"/cart"}>
-            <a className="m-2 rounded-md border border-button bg-transparent text-center text-kym4 hover:bg-button">
-              Ver cesta
-            </a>
-          </Link>
-        </div>
-        <hr className=" border-1 mt-5 w-full border-gray-400 py-2"></hr>
-        {/* Bill -> Products */}
-        <div className="grid pl-1">
+        <div className="grid ">
           {myCart ? (
             myCart.productList.map((cartProduct) => (
               <div key={cartProduct.productId}>
-                <div className="font-small mb-4 grid grid-cols-[40%_30%_30%] items-center">
+                <div className="my-4 mb-4 grid grid-cols-[40%_30%_30%] items-center px-2 text-sm font-light">
                   <div className="flex flex-row items-center gap-2">
                     <Image
                       className="rounded-md"
@@ -47,11 +53,11 @@ function DropDownCart() {
                     {cartProduct.amount}{" "}
                     {cartProduct.product.Edible ? "g" : "u"}
                   </div>
-                  <span className="justify-self-end">
+                  <span className="justify-self-end font-normal">
                     {cartProduct.price.toFixed(2)} €
                   </span>
                 </div>
-                <hr className=" border-1 mt-5 border-gray-400 py-2"></hr>
+                <hr className=" border-1 mt-5 border-gray-400 "></hr>
               </div>
             ))
           ) : (
