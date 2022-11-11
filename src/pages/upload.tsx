03261e8c8da5693f8ab82f1d92e62beea6ade3af
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-11-11 19:32:01 +0100

    componente subir foto

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -1,42 +1,25 @@
-import { supabaseStorage, supabaseUrl } from "@utils/supabase";
-import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { UploadImage } from "@components/ui/UploadImage";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Upload() {
   const [imageURL, setImageURL] = useState("");
-  const uploadPhoto = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.currentTarget.files?.item(0);
-    if (!file) return;
-
-    const pathFile = `images/${uuidv4()}-${file.name}`;
-    const { data, error } = await supabaseStorage.upload(pathFile, file);
-
-    if (!error) {
-      setImageURL(`${supabaseUrl}/storage/v1/object/public/images/${pathFile}`);
-    }
-
-    console.log({ data, error });
-  };
 
   return (
     <div className="flex min-h-screen min-w-full flex-col items-center justify-center gap-5">
-      <p>Upload a .png or .jpg image (max 1MB).</p>
-      <input
-        className="file-input w-full max-w-xs"
-        onChange={uploadPhoto}
-        type="file"
-        accept="image/png, image/jpeg"
-      />
+      <UploadImage setImageURL={setImageURL} />
       {imageURL && (
-        <Image
-          alt={imageURL}
-          width={300}
-          height={300}
-          layout="intrinsic"
-          src={imageURL}
-          objectFit="cover"
-        />
+        <>
+          <Image
+            alt={imageURL}
+            width={300}
+            height={300}
+            layout="intrinsic"
+            src={imageURL}
+            objectFit="cover"
+          />
+          <p>{imageURL}</p>
+        </>
       )}
     </div>
   );
