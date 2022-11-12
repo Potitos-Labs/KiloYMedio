e722b2abc5025c052c4580ac17622cdf0961931f
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-11-12 17:10:19 +0100

    jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,12 @@ const config: Config = {
   clearMocks: true,
   preset: "ts-jest",
   setupFiles: ["dotenv/config"],
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@server/(.*)$": "<rootDir>/src/server/$1",
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
+  },
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/src/server/db/singleton.ts"],
   verbose: true,
