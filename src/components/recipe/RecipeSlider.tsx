c99bf9c660f9c1d14f8b13b7ce417d545da44c8e
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-11-12 15:12:57 +0100

    cambio de nombre de arrows

--- src/components/recipe/RecipeSlider.tsx
+++ src/components/recipe/RecipeSlider.tsx
@@ -1,7 +1,7 @@
 import { ScrollMenu } from "react-horizontal-scrolling-menu";
 
 import { RecipeCard } from "./RecipeCard";
-import { LeftArrow, RightArrow } from "./arrows";
+import { LeftArrow, RightArrow } from "./Arrows";
 
 const RecipeDisplayer = ({
   title,
