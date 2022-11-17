89b1e54006ca48e0359c5c9c6bcd640caa3a0c1f
piliv4 <79758821+piliv4@users.noreply.github.com>
2022-11-17 21:19:28 +0100

    ugly ass banner

--- src/components/recipe/RecipeSlider.tsx
+++ src/components/recipe/RecipeSlider.tsx
@@ -1,38 +1,44 @@
-import { ScrollMenu } from "react-horizontal-scrolling-menu";
-import { LeftArrow, RightArrow } from "./Arrows";
-import LoadingCard from "./LoadingCard";
-import { RecipeCard } from "./RecipeCard";
-
 const RecipeDisplayer = ({
   recipes,
 }: {
   recipes?: { id: string; name: string; imageURL: string; userId: string }[];
 }) => {
   return (
-    <div className="mx-6 mt-6 mb-8">
-      <div className="rounded-md ">
-        <ScrollMenu
-          LeftArrow={LeftArrow}
-          RightArrow={RightArrow}
-          options={{
-            ratio: 0.9,
-            rootMargin: "5px",
-            threshold: [0.01, 0.05, 0.5, 0.75, 0.95, 1],
-          }}
-        >
-          {recipes
-            ? recipes.map((recipe) => (
-                <RecipeCard
-                  id={recipe.id}
-                  name={recipe.name}
-                  ratings={4}
-                  imageURL={recipe.imageURL}
-                  authorID={recipe.userId}
-                  key={recipe.id}
-                ></RecipeCard>
-              ))
-            : [...Array(10)].map((e, i) => <LoadingCard key={i} />)}
-        </ScrollMenu>
+    <div className="mx-5 mt-8 mb-6">
+      <div className="carousel w-full">
+        {recipes &&
+          recipes.map((recipe, index) => (
+            <div
+              key={index}
+              id={`index${index}`}
+              className="carousel-item relative h-36 w-full"
+            >
+              <img src={recipe.imageURL} className="w-full" />
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                {index > 0 ? (
+                  <a href={`#index${index - 1}`} className="btn btn-circle">
+                    ❮
+                  </a>
+                ) : (
+                  <a
+                    href={`#index${recipes.length - 1}`}
+                    className="btn btn-circle"
+                  >
+                    ❮
+                  </a>
+                )}
+                {index < recipes.length ? (
+                  <a href={`#index${index + 1}`} className="btn btn-circle">
+                    ❯
+                  </a>
+                ) : (
+                  <a href={`#index${0}`} className="btn btn-circle">
+                    ❯
+                  </a>
+                )}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
