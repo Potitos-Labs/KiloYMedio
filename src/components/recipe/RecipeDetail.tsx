b1fc7be4a008543dd6f0dc6128913483d24f921b
martalopeez <martalopeez88@gmail.com>
2022-11-23 18:38:49 +0100

    inicio recipedetail

--- src/components/recipe/RecipeDetail.tsx
+++ src/components/recipe/RecipeDetail.tsx
@@ -1,5 +1,6 @@
-import Heart from "@components/Heart";
 import Product from "@components/product/Product";
+
+import Image from "next/image";
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
@@ -9,6 +10,7 @@ import { toast } from "react-toastify";
 import { trpc } from "../../utils/trpc";
 import DotMenu from "../DotMenu";
 import Stars from "../Stars";
+import { IoBookmarkOutline, IoBookmark } from "react-icons/io5";
 
 const RecipeDetail = ({ id }: { id: string }) => {
   const { data: recipe } = trpc.recipe.getById.useQuery({ id });
@@ -65,132 +67,146 @@ const RecipeDetail = ({ id }: { id: string }) => {
   }
 
   return (
-    <div className="mx-8 mt-16 sm:mx-28">
-      {/* Upper section */}
-      <div className="mb-14 flex flex-col gap-14 lg:flex-row">
-        <img
-          className="max-h-64 min-h-full w-96 rounded-md"
-          src={recipe?.imageURL}
-          alt="not found"
-        ></img>
-
-        {/* Title, ratting, heart and description */}
-        <div className="w-full">
-          {/* Name, ratting, dot/heart */}
-          <div className="flex w-full gap-4">
-            <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
-              <h1 className="text-2xl font-bold uppercase">{recipe?.name}</h1>
-              <Stars average={4}></Stars>
+    <div>
+      <div className="mx-8 mt-24 sm:mx-20">
+        <div className="my-20 rounded-lg bg-[#F8F3ED] p-14">
+          {/* Upper section */}
+          <div className="flex">
+            <div className="mr-10 w-full gap-4">
+              <div className="mb-20 flex gap-4 lg:flex-row lg:items-center">
+                <h1 className="text-2xl font-bold uppercase">{recipe?.name}</h1>
+                {(session?.user?.id == recipe?.userId || isAdmin) && (
+                  <DotMenu
+                    id={id}
+                    name={recipe ? recipe.name : "Error Ocurred"}
+                    type="receta"
+                    updateFunction={editRecipe}
+                    deleteFunction={deleteRecipe}
+                  />
+                )}
+              </div>
+              {/* Features */}
+              <div className="mr-20">
+                <h2 className="flex justify-between">
+                  <span>Tiempo de preparación</span>
+                  {recipe?.timeSpan} min
+                </h2>
+                <hr className="my-4 border-[#0000004D]"></hr>
+                <h2 className="flex justify-between">
+                  <span>Cocinar</span>
+                  {recipe?.timeSpan} min {/* Por hacer */}
+                </h2>
+                <hr className="my-4 border-[#0000004D]"></hr>
+                <h2 className="flex justify-between">
+                  <span>Tiempo total</span>
+                  {recipe?.timeSpan} min {/* Por hacer */}
+                </h2>
+                <hr className="my-4 border-[#0000004D]"></hr>
+                <h2 className="flex justify-between">
+                  <span>Raciones</span> {recipe?.portions}
+                </h2>
+                <hr className="my-4 border-[#0000004D]"></hr>
+                <h2 className="flex justify-between">
+                  <span>Dificultad</span> {recipe?.difficulty}
+                </h2>
+                <hr className="my-4 border-[#0000004D]"></hr>
+              </div>
+              {/* End Features */}
             </div>
-            {(session?.user?.id == recipe?.userId || isAdmin) && (
-              <DotMenu
-                id={id}
-                name={recipe ? recipe.name : "Error Ocurred"}
-                type="receta"
-                updateFunction={editRecipe}
-                deleteFunction={deleteRecipe}
-              />
-            )}
             <div>
-              {!isAdmin && (
-                <Heart
-                  id={id}
-                  favorite={recipe?.isFav}
-                  addFavorite={saveRecipe}
-                  removeFavorite={unsaveRecipe}
-                />
-              )}
+              <Image
+                className="rounded-md"
+                src={recipe?.imageURL ?? ""}
+                alt="not found"
+                width="300"
+                height="380"
+                layout="fixed"
+                objectFit="cover"
+              ></Image>
+              <button
+                onClick={recipe?.isFav ? unsaveRecipe : saveRecipe}
+                className="absolute top-[480px] right-40 z-10 flex gap-2 rounded-full bg-[#F8F3ED] px-4 pb-1 hover:bg-[#212529] hover:text-white"
+              >
+                {recipe?.isFav ? "eliminar" : "guardar"}
+                {recipe?.isFav ? (
+                  <IoBookmark className="mt-1.5" />
+                ) : (
+                  <IoBookmarkOutline className="mt-1.5" />
+                )}
+              </button>
             </div>
           </div>
-          {/* End Name, ratting, dot/heart */}
-          <div className="my-8 text-justify">{recipe?.description}</div>
+          {/* End Upper section */}
 
-          {/* Features */}
-          <hr className="border-1 my-5 border-orange-200"></hr>
-          <div className="grid grid-cols-[33%_37%_29%]">
-            <h2 className="flex flex-col">
-              <span className="text-xs">RACIONES</span> {recipe?.portions}
-            </h2>
-            <h2 className="flex flex-col">
-              <span className="text-xs">DIFICULTAD</span> {recipe?.difficulty}
-            </h2>
-            <h2 className="flex flex-col">
-              <span className="text-xs">TIEMPO</span> {recipe?.timeSpan} min.
-            </h2>
-          </div>
-          <hr className="border-1 my-5 border-orange-200"></hr>
-          {/* End Features */}
-        </div>
-        {/* End Title, ratting, button and description */}
-      </div>
-      {/* End Upper section */}
+          {/* Ingredients and Directions */}
+          <div className="mt-20 mb-10 grid grid-cols-1 md:grid-cols-2 md:gap-4">
+            <div className="mx-10">
+              <h2 className="mb-4 border-b-[1px] border-[#0000004D] text-lg font-bold">
+                INGREDIENTES
+              </h2>
+              <div className="mb-4 md:pr-14">
+                <ul className="ml-5 list-disc space-y-2">
+                  {units &&
+                    ingredients?.map((i) => {
+                      return (
+                        <li key={""}>
+                          <div className="flex gap-2">
+                            <span>{i.amount}</span>
+                            <span>{units[i.unit]}</span>
+                            <p className="lowercase">de {i.Ingredient.name}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                </ul>
+              </div>
+            </div>
 
-      {/* Ingredients and Directions */}
-      <div className="mb-40 grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-4">
-        <div>
-          <h2 className="mb-2 text-lg font-bold">INGREDIENTES</h2>
-          <div className="mb-4 md:pr-14">
-            {units &&
-              ingredients?.map((i) => {
-                return (
-                  <div key={"id"}>
-                    <hr className="border-1 my-5 border-orange-200"></hr>
-                    <div className="flex justify-between">
-                      <p className="first-letter:uppercase">
-                        {i.Ingredient.name}
-                      </p>
-                      <div className="flex gap-2">
-                        <span>{i.amount}</span>
-                        <span>{units[i.unit]}</span>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
+            <div className="mx-10">
+              <h2 className="mb-4 border-b-[1px] border-[#0000004D] text-lg font-bold">
+                PASOS
+              </h2>
+              <div>
+                <ol className="list-inside list-decimal space-y-6">
+                  {directions?.map((d) => {
+                    return (
+                      <li key={d.number} className="first-letter:uppercase">
+                        {d.direction}
+                      </li>
+                    );
+                  })}
+                </ol>
+              </div>
+            </div>
           </div>
         </div>
+        {/* End Ingredients and Directions */}
 
-        <div>
-          <h2 className="mb-6 text-lg font-bold md:mb-10">INSTRUCCIONES</h2>
-          <div>
-            <ol key={"id"} className="list-inside list-decimal">
-              {directions?.map((d) => {
-                return (
-                  <li
-                    key={d.number}
-                    className="first-letter:uppercase marker:font-bold marker:text-kym2"
-                  >
-                    {d.direction}
-                  </li>
-                );
-              })}
-            </ol>
+        {/* Products */}
+        <div className="mb-14 rounded-lg bg-[#212529]">
+          <h2 className="mb-10 text-2xl font-bold text-white">
+            DIRECTO A TU CESTA
+          </h2>
+          <div className="grid w-full grid-cols-1 justify-center gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            {ingredients ? (
+              ingredients.map((i) => {
+                if (i.Ingredient.Edible)
+                  return (
+                    <Product product={i.Ingredient.Edible.product}></Product>
+                  );
+              })
+            ) : (
+              <p className="font-semibold text-kym4">Cargando...</p>
+            )}
           </div>
         </div>
+        {/* End Products */}
       </div>
-      {/* End Ingredients and Directions */}
-
-      {/* Products */}
-      <div className="mb-14 text-center">
-        <h2 className="text-lg font-bold">COMPRA LOS INGREDIENTES</h2>
-        <hr className="border-3 mt-5 mb-10 border-orange-200"></hr>
-        <div className="grid w-full grid-cols-1 justify-center gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-          {ingredients ? (
-            ingredients.map((i) => {
-              if (i.Ingredient.Edible)
-                return (
-                  <Product product={i.Ingredient.Edible.product}></Product>
-                );
-            })
-          ) : (
-            <p className="font-semibold text-kym4">Cargando...</p>
-          )}
-        </div>
-      </div>
-      {/* End Products */}
-
       {/* Footer */}
-      <hr className="border-1 my-24 border-orange-200"></hr>
+      <div className="bg-[#F8F3ED] p-8">
+        <h2 className="text-2xl font-bold">COMENTARIOS</h2>
+        <Stars average={4}></Stars>
+      </div>
     </div>
   );
 };
