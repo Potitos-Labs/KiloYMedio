13897217d6b061123ef8d932fd0851c4dc8cc12a
JOHNNY-22 <johnny.altes1@gmail.com>
2022-11-26 23:29:57 +0100

    PEstañas primcipales acabadas

---
+++ src/components/AdminPage.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+function AdminPage() {
+  return (
+    <div className="bg-accent">
+      <div className="flex justify-between pr-14">
+        <div className="mt-32 ml-10 flex w-48 flex-col gap-6">
+          <Link href="/admin/register">
+            <button className="rounded-full bg-base-100  py-3 font-satoshiBold text-xs text-base-content">
+              registrar usuario
+            </button>
+          </Link>
+          <Link href="/product/create">
+            <button className="rounded-full bg-base-100  py-3 font-satoshiBold text-xs text-base-content">
+              crear producto
+            </button>
+          </Link>
+          <Link href="/product/create">
+            <button className="rounded-full bg-base-100  py-3 font-satoshiBold text-xs text-base-content">
+              crear taller
+            </button>
+          </Link>
+        </div>
+        <Image src="/img/fondoCucharasSinFondo.png" width="700" height="400" />
+      </div>
+    </div>
+  );
+}
+
+export default AdminPage;
