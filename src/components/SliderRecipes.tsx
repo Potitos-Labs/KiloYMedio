ccb38f1fdc784e32eb27a36d17ee1fb72d44a336
JOHNNY-22 <johnny.altes1@gmail.com>
2022-12-01 11:48:11 +0100

    slider mejorado

--- src/components/SliderRecipes.tsx
+++ src/components/SliderRecipes.tsx
@@ -26,20 +26,21 @@ function SliderRecipes({
       <div className="flex h-auto w-full items-center justify-between py-24 px-4 sm:py-8">
         {/* Carousel for desktop and large size devices */}
         <CarouselProvider
-          className=" flex h-full w-full flex-col"
+          className="hidden h-full w-full md:flex md:flex-col"
           naturalSlideWidth={100}
           naturalSlideHeight={150}
           isIntrinsicHeight={true}
           totalSlides={recipes?.length ?? 0}
           visibleSlides={isBig ? 2 : 4}
           step={isBig ? 1 : 2}
+          infinite={true}
         >
           <div className=" flex w-full flex-col items-center justify-between">
             <div className="mx-auto h-full w-full">
               <Slider>
                 <div
                   id="slider"
-                  className="flex h-full items-center justify-start gap-14 transition duration-700 ease-out md:gap-6 lg:gap-8"
+                  className="flex h-full items-center justify-between gap-8 transition duration-700 ease-out"
                 >
                   {recipes?.map((r, index) => {
                     if (!isBig) {
@@ -75,7 +76,7 @@ function SliderRecipes({
               <ButtonBack
                 role="button"
                 aria-label="slide backward"
-                className="left-0 z-30 ml-8 cursor-pointer focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
+                className="left-0 z-30 ml-8 cursor-pointer"
                 id="prev"
               >
                 <BsFillArrowLeftSquareFill className="h-[25px] w-[50px] rounded-sm" />
@@ -83,7 +84,7 @@ function SliderRecipes({
               <ButtonNext
                 role="button"
                 aria-label="slide forward"
-                className="right-0 z-30 mr-8 focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
+                className="right-0 z-30 mr-8 "
                 id="next"
               >
                 <BsFillArrowRightSquareFill className="h-[25px] w-[50px] rounded-sm" />
@@ -93,6 +94,59 @@ function SliderRecipes({
         </CarouselProvider>
 
         {/* Carousel for tablet and medium size devices */}
+        <CarouselProvider
+          className=" flex h-full w-full flex-col sm:hidden"
+          naturalSlideWidth={100}
+          naturalSlideHeight={200}
+          isIntrinsicHeight={true}
+          totalSlides={recipes?.length ?? 0}
+          visibleSlides={2}
+          step={1}
+          infinite={true}
+        >
+          <div className=" flex w-full flex-col items-center justify-between">
+            <div className="mx-auto h-full w-full">
+              <Slider>
+                <div
+                  id="slider"
+                  className="flex h-full items-center  gap-6 transition duration-700 ease-out"
+                >
+                  {recipes?.map((r, index) => {
+                    return (
+                      <Slide index={index} key={""}>
+                        <SmallRecipeCard
+                          key={r.id}
+                          id={r.id}
+                          name={r.name}
+                          imageURL={r.imageURL}
+                          authorID={r.userId}
+                        />
+                      </Slide>
+                    );
+                  })}
+                </div>
+              </Slider>
+            </div>
+            <div className="flex w-full justify-end py-10">
+              <ButtonBack
+                role="button"
+                aria-label="slide backward"
+                className="left-0 z-30 ml-8 cursor-pointer"
+                id="prev"
+              >
+                <BsFillArrowLeftSquareFill className="h-[25px] w-[50px] rounded-sm" />
+              </ButtonBack>
+              <ButtonNext
+                role="button"
+                aria-label="slide forward"
+                className="right-0 z-30 mr-8"
+                id="next"
+              >
+                <BsFillArrowRightSquareFill className="h-[25px] w-[50px] rounded-sm" />
+              </ButtonNext>
+            </div>
+          </div>
+        </CarouselProvider>
       </div>
     </div>
   );
