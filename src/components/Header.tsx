acc60835b23d954c0b23c1332cf9b9c56f7fdd74
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-12-11 21:31:58 +0100

    logos con svgs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import NavBar from "./navbar/NavBar";
 import NavBarClient from "./navbar/NavBarClient";
 import router from "next/router";
+import clsx from "clsx";
 
 interface HeaderProps {
   bgLight: boolean;
@@ -20,12 +21,18 @@ const Header = ({ bgLight, textDark }: HeaderProps) => {
     <div
       className={`${bgColor} ${textColor} z-20 justify-between rounded-b-3xl p-2 lg:p-0`}
     >
-      <div className="absolute left-8 top-4 flex cursor-pointer lg:hidden">
+      <div
+        className={clsx(
+          "absolute left-8 flex cursor-pointer lg:hidden",
+          textDark ? "top-1" : "top-2",
+        )}
+      >
         <Image
-          src="/img/logopequeño.png"
+          src={textDark ? "/logo pequeño.svg" : "/logo pequeño-blanco.svg"}
           alt="not found"
-          width="24"
-          height="28"
+          className=""
+          width="40"
+          height="50"
           onClick={() => router.push("/")}
         ></Image>
       </div>
@@ -33,7 +40,16 @@ const Header = ({ bgLight, textDark }: HeaderProps) => {
         <NavBarClient />
         <Link href={`/`}>
           <h3 className="my-3 hidden w-[180px] cursor-pointer font-raleway text-lg lg:flex">
-            kilo y medio
+            <Image
+              src={
+                textDark
+                  ? "/logo sin subtitulo.svg"
+                  : "/logo sin subtitulo-blanco.svg"
+              }
+              width={180}
+              height={50}
+              alt="kilo y medio"
+            ></Image>
           </h3>
         </Link>
         <NavBar></NavBar>
