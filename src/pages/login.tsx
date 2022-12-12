27cb65ca510e3d8318edf79ab2c80c31c58587c2
martalopeez <martalopeez88@gmail.com>
2022-12-12 18:13:01 +0100

    más login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -34,7 +34,6 @@ const SignIn: NextPage = () => {
     emailNotExists == ""
   ) {
     setEmailNotExists("Email y/o contraseña inválido");
-    //router.replace("/login", undefined, { shallow: true });
   }
   console.log(router.basePath);
   const onSubmit = useCallback(async (data: ILogin) => {
@@ -56,14 +55,9 @@ const SignIn: NextPage = () => {
       <input type="checkbox" id="my-modal" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box">
-          <h3 className="mt-[10px] ml-[10px] text-lg font-bold">
-            Haber estudiado
-          </h3>
+          <h3 className="mt-2 ml-2 text-lg">Haber estudiado</h3>
           <div className="modal-action">
-            <label
-              htmlFor="my-modal"
-              className="btn btn-lg w-[100px] text-base-100"
-            >
+            <label htmlFor="my-modal" className="btn btn-lg w-24 text-base-100">
               F
             </label>
           </div>
@@ -83,13 +77,13 @@ const SignIn: NextPage = () => {
           </Link>
         </div>
         <div className="flex flex-col md:items-end">
-          <div className="rounded-box mx-2 mb-10 mt-[20px] h-full border-[1px] bg-base-100 md:mr-[70px] md:h-[760px] md:w-[540px]">
+          <div className="rounded-box mx-2 mb-10 mt-5 h-full border-[1px] bg-base-100 sm:mx-6 md:mr-16 md:h-[760px] md:w-[540px]">
             {/* Text */}
             <div className="text-center">
-              <p className="ml-[15px] mr-[175px] mt-[30px] w-[335px] font-raleway text-[43px] md:mt-[80px] md:ml-[30px]">
+              <p className="ml-4 mr-44 mt-8 w-[335px] font-raleway text-[43px] md:mt-20 md:ml-8">
                 ¡Hola de nuevo!
               </p>
-              <p className="ml-[16px] mr-[17px] mt-[30px] text-left text-sm md:ml-[33px]">
+              <p className="mx-4 mt-8 text-left text-sm md:ml-8">
                 ¿Aún no tienes una cuenta? Únete a kilo y medio {""}
                 <Link href={`/register?prev=${router.query.prev?.toString()}`}>
                   <b className="cursor-pointer font-satoshiBold">
@@ -102,11 +96,11 @@ const SignIn: NextPage = () => {
             {/* End Text */}
             <form onSubmit={handleSubmit(onSubmit)}>
               {/* Email + password */}
-              <div className="responsive mt-[30px] flex flex-col items-center gap-[19px] md:mt-[67px]">
-                <div className="mx-[30px]">
+              <div className="responsive mt-8 flex flex-col items-center gap-5 md:mt-16">
+                <div className="mx-8">
                   <div className="flex max-w-[480px]">
                     <input
-                      className="input input-bordered h-[60px] w-[350px] rounded-[30px] border-base-300 text-sm text-base-300 md:w-[480px]"
+                      className="input input-bordered h-16 w-[350px] rounded-full border-base-300 text-sm text-base-300 md:w-[480px]"
                       type="email"
                       id="emailInput"
                       placeholder="E-mail"
@@ -116,24 +110,24 @@ const SignIn: NextPage = () => {
                     />
                   </div>
                   {errors.email && (
-                    <p className="ml-7 -mb-[16px] text-[14px] text-red-500">
+                    <p className="text-4 ml-7 -mb-4 text-red-500">
                       {errors.email.message}
                     </p>
                   )}
                 </div>
-                <div className="mx-[30px] md:w-[480px]">
+                <div className="mx-8 md:w-[480px]">
                   <div className="flex items-center justify-end">
-                    <label className="swap absolute mr-[30px] md:ml-[430px]">
+                    <label className="swap absolute mr-8 md:ml-[430px]">
                       <input
                         type="checkbox"
                         onClick={() => showHidePassword()}
                       ></input>
-                      <IoEyeSharp className="swap-on h-[25px] w-[25px]"></IoEyeSharp>
-                      <IoEyeOffSharp className="swap-off h-[25px] w-[25px]"></IoEyeOffSharp>
+                      <IoEyeSharp className="swap-on h-6 w-6"></IoEyeSharp>
+                      <IoEyeOffSharp className="swap-off h-6 w-6"></IoEyeOffSharp>
                     </label>
                     <input
                       type={showPassword ? "text" : "password"}
-                      className="input input-bordered h-[60px] w-[350px] rounded-[30px] border-base-300 text-sm text-base-300 md:w-[480px]"
+                      className="input input-bordered h-16 w-[350px] rounded-[30px] border-base-300 text-sm text-base-300 md:w-[480px]"
                       id="passwordInput"
                       placeholder="Contraseña"
                       {...register("password", {
@@ -142,19 +136,19 @@ const SignIn: NextPage = () => {
                     />
                   </div>
                   {errors.password && (
-                    <p className="ml-7 -mb-[16px] text-[14px] text-red-500">
+                    <p className="text-4 ml-7 -mb-4 text-red-500">
                       Contraseña inválida
                     </p>
                   )}
                   <div className="mt-[1px] w-full text-end">
                     <label
                       htmlFor="my-modal"
-                      className="ml-[327] cursor-pointer text-xs md:mr-[31px]"
+                      className="cursor-pointer text-xs md:mr-[31px]"
                     >
                       ¿Olvidaste tu contraseña?
                     </label>
                   </div>
-                  <p className="font-semibold text-red-500">{emailNotExists}</p>
+                  <p className="text-red-500">{emailNotExists}</p>
                 </div>
               </div>
               {/* End Email + password */}
@@ -162,14 +156,14 @@ const SignIn: NextPage = () => {
               <div className="flex flex-col items-center">
                 <button
                   type="submit"
-                  className="responsive btn btn-lg mx-[15px] mt-[69px] h-[60px] w-[350px] cursor-pointer text-sm text-base-100 transition duration-150 ease-in-out md:mx-[30px] md:w-[480px]"
+                  className="responsive btn btn-lg mx-4 mt-16 h-14 w-[350px] cursor-pointer text-sm text-base-100 transition duration-150 ease-in-out md:mx-8 md:w-[480px]"
                 >
                   Iniciar sesión
                 </button>
-                <hr className="mx-[16px] mt-[30px] flex h-0 w-[346px] border-spacing-0 border-base-content border-opacity-30 md:mx-[32px] md:w-[476px]"></hr>
-                <div className="mx-[15px] mt-[30px] mb-[15px] flex items-center align-middle md:mx-[30px] md:mb-0">
+                <hr className="mx-4 mt-7 flex h-0 w-[346px] border-spacing-0 border-base-300 border-opacity-30 md:mx-8 md:w-[476px]"></hr>
+                <div className="mx-8 mt-7 mb-4 flex items-center align-middle md:mb-0">
                   <button
-                    className="btn h-[60px] w-[350px] rounded-[30px] border-[1px] border-base-content bg-base-100 font-raleway text-sm hover:bg-base-100 md:w-[480px]"
+                    className="btn h-14 w-[350px] rounded-full border-[1px] border-base-content bg-base-100 font-raleway text-sm hover:bg-base-100 md:w-[480px]"
                     type="button"
                     style={{ transition: "all .15s ease" }}
                     onClick={() =>
@@ -180,7 +174,10 @@ const SignIn: NextPage = () => {
                   >
                     Iniciar sesión con Google
                   </button>
-                  <IoLogoGoogle className="absolute ml-[25px] h-[25px] w-[25px]"></IoLogoGoogle>
+                  <IoLogoGoogle
+                    className="absolute ml-6"
+                    size="25"
+                  ></IoLogoGoogle>
                 </div>
               </div>
               {/* End Buttons */}
