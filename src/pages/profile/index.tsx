b0a4ef326e21679cfd0fa67d9c005b02bd4ed321
Alicia Grao <agramel@inf.upv.es>
2022-12-14 13:46:57 +0100

    cosas de @piliv4

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 import MyProfile from "@components/profile/profileCards/MyProfile";
 import TinyText from "@components/profile/profileCards/TinyText";
-import Footer from "@components/profile/profileCards/Footer";
+import FooterProfile from "@components/profile/profileCards/FooterProfile";
 import CenterItem from "@components/profile/profileCards/CenterItem";
 import { trpc } from "@utils/trpc";
 
@@ -21,8 +21,8 @@ const Profile = () => {
 
   return (
     <Layout bgColor={"bg-base-100"} headerBgLight={true} headerTextDark={true}>
-      <div className="sm:first-letter mt-[26px]  mb-7 grid min-h-90% grid-cols-1 place-content-center lg:grid-cols-[58%_42%]  lg:place-content-start ">
-        <div className="px-6 sm:px-14 lg:pl-28">
+      <div className="sm:first-letter mt-[10px] mb-7 grid h-full min-h-90% grid-cols-1 place-content-center sm:mt-0 lg:grid-cols-[58%_42%]">
+        <div className="grid items-center px-6 align-middle sm:px-14 lg:pl-28">
           {client && <MyProfile image={client.image} name={client.name} />}
 
           <div className="mt-[20px] grid grid-cols-1 gap-2 sm:grid-cols-[40%_60%] sm:gap-4 sm:pr-4 ">
@@ -40,9 +40,10 @@ const Profile = () => {
             allergenList={allergenList}
             workshopList={workshopList}
           />
-          <Footer />
+          <FooterProfile />
         </div>
-        <div className="-mt-[-1  px]   relative  hidden max-h-[630px] items-start justify-start align-top lg:block">
+
+        <div className="-mt-[-1  px]   relative  hidden max-h-[800px] items-start justify-start align-top lg:block">
           <Image
             src="/img/bolitas.svg"
             alt="Mi imagen"
